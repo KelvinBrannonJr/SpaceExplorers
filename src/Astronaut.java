eaad95cd67ed8071d99258a78ac677e90c76c20d
Kelvin <Kelzjr56@gmail.com>
2024-04-01 22:24:14 -0400

    Created Astronaut class with defined private members and constructors

---
+++ src/Astronaut.java
@@ -0,0 +1,29 @@
+public class Astronaut {
+    // Astronaut private members
+    private String astronautName;
+    private String astronautRank;
+    private String homeCountry;
+    private String uniqueSkill;
+    private String shuttleName;
+
+
+    // Default Astronaut Constructor
+    public Astronaut() {
+        this.astronautName = "No Name";
+        this.astronautRank = "No Rank";
+        this.homeCountry = "No Country of Origin";
+        this.uniqueSkill = "No specialty skill";
+        this.shuttleName = "No shuttle name";
+    }
+
+    // Astronaut Overloaded Constructor
+    public Astronaut(String name, String rank, String country, String skill, String shuttle) {
+        this.astronautName = name;
+        this.astronautRank = rank;
+        this.homeCountry = country;
+        this.uniqueSkill = skill;
+        this.shuttleName = shuttle;
+    }
+
+
+}
