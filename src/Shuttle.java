96bd5fa4d29254e2bb99bda5b52d59897093753a
Kelvin <Kelzjr56@gmail.com>
2024-04-13 23:38:59 -0400

    Implemented shuttle default and overloaded constructors

--- src/Shuttle.java
+++ src/Shuttle.java
@@ -1,10 +1,29 @@
 public class Shuttle {
 
+    // Shuttle private members
     private String shuttleName;
     private String countryOrigin;
     private String researchType;
     private int numOfRooms;
     private boolean hasRover;
+
+    // Default Shuttle Constructors
+    public Shuttle() {
+        this.shuttleName = "Shuttle Unknown";
+        this.countryOrigin = "No Country";
+        this.researchType = "Research Unknown";
+        this.numOfRooms = 0;
+        this.hasRover = false;
+    }
+
+    // Overloaded Shuttle Constructor
+    public Shuttle(String name, String origin, String research, int roomNums, boolean hasRov) {
+        this.shuttleName = name;
+        this.countryOrigin = origin;
+        this.researchType = research;
+        this.numOfRooms = roomNums;
+        this.hasRover = hasRov;
+    }
 
 
 }
