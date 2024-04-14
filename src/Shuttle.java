e2f17a8a081b9dc9d27e3961c2c8229260906692
Kelvin <Kelzjr56@gmail.com>
2024-04-13 23:26:15 -0400

    Created Shuttle class and declared private class members

---
+++ src/Shuttle.java
@@ -0,0 +1,10 @@
+public class Shuttle {
+
+    private String shuttleName;
+    private String countryOrigin;
+    private String researchType;
+    private int numOfRooms;
+    private boolean hasRover;
+
+
+}
