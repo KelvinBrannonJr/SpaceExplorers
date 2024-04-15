8c5ebbbb26b8acc70e762c8aaa9a5a9cc94ef3e2
Kelvin <Kelzjr56@gmail.com>
2024-04-14 23:51:40 -0400

    Created Rover class and declared class members

---
+++ src/Rover.java
@@ -0,0 +1,14 @@
+public class Rover {
+
+    private String roverName;
+
+    enum SensorType {
+        DOPPLER,
+        INFERRED,
+        MICROBIAL,
+        Topography
+    }
+    private double batteryRange;
+    private boolean hasSolarCharger;
+
+}
