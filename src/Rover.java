ab82586f79ea591686ba7994c0e89ef39d71f479
Kelvin <Kelzjr56@gmail.com>
2024-04-15 22:32:51 -0400

    Implemented Rover class enum sensorType and class constructors

--- src/Rover.java
+++ src/Rover.java
@@ -1,14 +1,36 @@
 public class Rover {
 
     private String roverName;
-
-    enum SensorType {
-        DOPPLER,
-        INFERRED,
-        MICROBIAL,
-        Topography
-    }
     private double batteryRange;
     private boolean hasSolarCharger;
+
+    public enum SensorType {
+        DEFAULT("Default"),
+        DOPPLER("Doppler"),
+        INFERRED("InferRed"),
+        MICROBIAL("Microbial"),
+        SEISMIC("Seismic");
+
+        private String sensorTypeDesc;
+
+        SensorType(String sensorTypeCode) {
+            this.sensorTypeDesc = sensorTypeCode;
+        }
+        public String getSensorType() {
+            return sensorTypeDesc;
+        }
+    }
+
+    public void Rover() {
+        this.roverName = "None";
+        this.batteryRange = 0.0;
+        this.hasSolarCharger = false;
+    }
+
+    public void Rover(String roverName, double batteryRange, boolean hasSolarCharger) {
+        this.roverName = roverName;
+        this.batteryRange = batteryRange;
+        this.hasSolarCharger = hasSolarCharger;
+    }
 
 }
