368fcc691028f395b09e87789e7c34b933c8c80a
Kelvin <Kelzjr56@gmail.com>
2024-04-23 23:40:45 -0400

    Completed test-cases for Astronaut class and all Tests: PASSED

--- test/java/AstronautTest.java
+++ test/java/AstronautTest.java
@@ -50,36 +50,42 @@ public class AstronautTest {
         @Test
         public void getAstronautName() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setAstronautName("testName");
+            assertTrue(astronaut.getAstronautName().equals("testName"));
         }
 
         @Test
         public void getAstronautAge() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setAstronautAge(25);
+            assertTrue(astronaut.getAstronautAge() == 25);
         }
 
         @Test
         public void getAstronautRank() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setAstronautRank("Newbie");
+            assertTrue(astronaut.getAstronautRank().equals("Newbie"));
         }
 
         @Test
         public void getHomeCountry() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setHomeCountry("testEarth");
+            assertTrue(astronaut.getHomeCountry().equals("testEarth"));
         }
 
         @Test
         public void getUniqueSkill() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setUniqueSkill("testSkill");
+            assertTrue(astronaut.getUniqueSkill().equals("testSkill"));
         }
 
         @Test
         public void getShuttleName() {
             Astronaut astronaut = new Astronaut();
-
+            astronaut.setShuttleName("testShuttle");
+            assertTrue(astronaut.getShuttleName().equals("testShuttle"));
         }
 }
