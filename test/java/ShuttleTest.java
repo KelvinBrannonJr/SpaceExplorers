3aaf2c9899558ec7024dcd226265a8b53ed6253e
Kelvin <Kelzjr56@gmail.com>
2024-04-24 21:46:00 -0400

    Completed test-cases for Shuttle class and all Tests: PASSED

--- test/java/ShuttleTest.java
+++ test/java/ShuttleTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ShuttleTest {
@@ -63,36 +64,42 @@ public class ShuttleTest {
     @Test
     void getCountryOrigin() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setCountryOrigin("testCountry");
+        assertTrue(shuttle.getCountryOrigin().equals("testCountry"));
     }
 
     @Test
-    void getResearchType() {
+    void getResearchPurpose() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setResearchPurpose("testResearch");
+        assertTrue(shuttle.getResearchPurpose().equals("testResearch"));
     }
 
     @Test
     void getNumOfRooms() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setNumOfRooms(3);
+        assertTrue(shuttle.getNumOfRooms() == 3);
     }
 
     @Test
     void getHasRover() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setHasRover(false);
+        assertFalse(shuttle.getHasRover());
     }
 
     @Test
     void getThrustPower() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setThrustPower(1000.0);
+        assertTrue(shuttle.getThrustPower() == 1000.0);
     }
 
     @Test
     void getFuelCapacity() {
         Shuttle shuttle = new Shuttle();
-
+        shuttle.setFuelCapacity(100.0);
+        assertTrue(shuttle.getFuelCapacity() == 100.0);
     }
 }
