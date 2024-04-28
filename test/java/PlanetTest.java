d40f14fc10eb6969abb5444882e1db7c56fe12a6
Kelvin <Kelzjr56@gmail.com>
2024-04-27 23:07:47 -0400

    Created PlanetTest class and declared test class methods

---
+++ test/java/PlanetTest.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanetTest {
+
+    @Test
+    void setPlanetName() {
+    }
+
+    @Test
+    void setPlanetMass() {
+    }
+
+    @Test
+    void setGravity() {
+    }
+
+    @Test
+    void setPlanetTypes() {
+    }
+
+    @Test
+    void setHasAtmosphere() {
+    }
+
+    @Test
+    void setAtmosphereTemp() {
+    }
+
+    @Test
+    void setHasSurface() {
+    }
+
+    @Test
+    void setSurfaceTemp() {
+    }
+
+    @Test
+    void setAbundantResource() {
+    }
+
+    @Test
+    void setHabitableForHumans() {
+    }
+
+    @Test
+    void getPlanetName() {
+    }
+
+    @Test
+    void getPlanetMass() {
+    }
+
+    @Test
+    void getGravity() {
+    }
+
+    @Test
+    void getPlanetTypes() {
+    }
+
+    @Test
+    void getHasAtmosphere() {
+    }
+
+    @Test
+    void getAtmosphereTemp() {
+    }
+
+    @Test
+    void getHasSurface() {
+    }
+
+    @Test
+    void getSurfaceTemp() {
+    }
+
+    @Test
+    void getAbundantResource() {
+    }
+
+    @Test
+    void getIsHabitableForHumans() {
+    }
+}
