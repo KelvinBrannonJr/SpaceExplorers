d69491a32b193748f1a5886a75fb66272780b9ff
Kelvin <Kelzjr56@gmail.com>
2024-04-27 22:54:11 -0400

    Created RoverTest for Rover class and implemented setters

--- test/java/RoverTest.java
+++ test/java/RoverTest.java
@@ -6,22 +6,37 @@ class RoverTest {
 
     @Test
     void setRoverName() {
+        Rover rover = new Rover();
+        rover.setRoverName("testRover");
+        assertTrue(rover.getRoverName().equals("testRover"));
     }
 
     @Test
     void setBatteryRange() {
+        Rover rover = new Rover();
+        rover.setBatteryRange(45.0);
+        assertTrue(rover.getBatteryRange() == 45.0);
     }
 
     @Test
     void setHasSolarCharger() {
+        Rover rover = new Rover();
+        rover.setHasSolarCharger(false);
+        assertFalse(rover.getHasSolarCharger());
     }
 
     @Test
     void setSensorTypes() {
+        Rover rover = new Rover();
+        rover.setSensorTypes(Rover.SensorTypes.DEFAULT);
+        assertTrue(rover.getSensorTypes() == Rover.SensorTypes.DEFAULT);
     }
 
     @Test
     void setMobilityTypes() {
+        Rover rover = new Rover();
+        rover.setMobilityTypes(Rover.MobilityTypes.DEFAULT);
+        assertTrue(rover.getMobilityTypes() == Rover.MobilityTypes.DEFAULT);
     }
 
     @Test
