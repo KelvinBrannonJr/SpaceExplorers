04a242d62eb1fae943295f88d7bdad8d0b03229a
Kelvin <Kelzjr56@gmail.com>
2024-04-29 22:43:55 -0400

    Completed unit-test getters for PlanetTest class, all tests: PASSED

--- test/java/PlanetTest.java
+++ test/java/PlanetTest.java
@@ -77,41 +77,71 @@ class PlanetTest {
 
     @Test
     void getPlanetName() {
+        Planet planet = new Planet();
+        planet.setPlanetName("testName");
+        assertTrue(planet.getPlanetName().equals("testName"));
     }
 
     @Test
     void getPlanetMass() {
+        Planet planet = new Planet();
+        planet.setPlanetMass(2.09e21);
+        assertTrue(planet.getPlanetMass() == 2.09e21);
     }
 
     @Test
     void getGravity() {
+        Planet planet = new Planet();
+        planet.setGravity(2.4);
+        assertTrue(planet.getGravity() == 2.4);
     }
 
     @Test
     void getPlanetTypes() {
+        Planet planet = new Planet();
+        planet.setPlanetTypes(Planet.PlanetTypes.DEFAULT);
+        assertTrue(planet.getPlanetTypes() == Planet.PlanetTypes.DEFAULT);
     }
 
     @Test
     void getHasAtmosphere() {
+        Planet planet = new Planet();
+        planet.setHasAtmosphere(true);
+        assertTrue(planet.getHasAtmosphere());
     }
 
     @Test
     void getAtmosphereTemp() {
+        Planet planet = new Planet();
+        planet.setAtmosphereTemp(63.2);
+        assertTrue(planet.getAtmosphereTemp() == 63.2);
     }
 
     @Test
     void getHasSurface() {
+        Planet planet = new Planet();
+        planet.setHasSurface(false);
+        assertFalse(planet.getHasSurface());
     }
 
     @Test
     void getSurfaceTemp() {
+        Planet planet = new Planet();
+        planet.setSurfaceTemp(876.3);
+        assertFalse(planet.getSurfaceTemp() == 375.5);
     }
 
     @Test
     void getAbundantResource() {
+        Planet planet = new Planet();
+        planet.setAbundantResource("testResource");
+        assertTrue(planet.getAbundantResource().equals("testResource"));
     }
 
     @Test
     void getIsHabitableForHumans() {
+        Planet planet = new Planet();
+        planet.setHabitableForHumans(false);
+        assertFalse(planet.getIsHabitableForHumans());
     }
 }
