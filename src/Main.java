09abd2f049cc0032ef0531536c3069dfb921c9ca
Kelvin <Kelzjr56@gmail.com>
2024-04-30 23:50:05 -0400

    Instantiated Astronaut object in main class but bloat would severe for getting user responses through input, come up with a cleaner design

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Testing");
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter the full name of the first crew member.");
+        String fullName = scn.nextLine();
+        Astronaut firstMember = new Astronaut();
+        firstMember.setAstronautName(fullName);
+        System.out.println(firstMember.getAstronautName());
+
     }
 }
