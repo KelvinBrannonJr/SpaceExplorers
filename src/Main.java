48785659d62b408c2dd6df00e84fef368c2bdeaa
Kelvin <Kelzjr56@gmail.com>
2024-05-05 23:26:40 -0400

    Created other astronaut objects

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
+        Astronaut astronaut1 = new Astronaut("Blake Johnson",25, "Candidate", "America", "Hvac Systems", "Emergence");
+        Astronaut astronaut2 = new Astronaut("Aiko Takahasi",37, "Astronaut", "Japan", "Atmospheric Chemist", "Hinoki");
+        Astronaut astronaut3 = new Astronaut("Oliver Thompson",51, "Astronaut", "Britain", "Guidance Systems", "Belfast");
+
         System.out.println("Enter the full name of the first crew member.");
         String fullName = scn.nextLine();
-        Astronaut firstMember = new Astronaut();
-        firstMember.setAstronautName(fullName);
-        System.out.println(firstMember.getAstronautName());
+        Astronaut astronaut4 = new Astronaut();
+        astronaut4.setAstronautName(fullName);
+        System.out.println(astronaut4.getAstronautName());
+
+
 
     }
 }
